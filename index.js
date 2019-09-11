7fdfc6e6a4d5c92daa71bfc76e8b0397c8a00e23
faahiero <fabriciosilvalp@outlook.com>
2019-09-11 13:49:12 -0300

    Configuraçao inicial do servidor

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT,()=>{
+    console.log('Servidor rodando');
+});
