eba18a172fd89dbc0e85a40866857f2dc790af25
faahiero <fabriciosilvalp@outlook.com>
2019-09-11 13:58:15 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT,()=>{
+    console.log('Servidor Rodando');
+    
+})
