b115350b48d8bb311ce54b95b7d29e7add33ee5c
Ladgelson <micaiasladgelsondasilva@gmail.com>
2019-09-17 19:38:09 -0300

    Pasta

---
+++ routes/extracaoRoute.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/extracoesModel');
+const addRefUserById = require('../controllers/jogo/addRefUserById');
+
+// GET
+router.get('/', async (req, res) => {
+    try {
+        const user = await User.find();
+        res.send(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// GET-by-ID
+router.get('/:id', getUser, (req, res) => {
+    res.send(res.user);
+});
+
+// POST
+router.post('/', async (req, res) => {
+    const user = new User({
+        jogo: "5d815d69c82b714788c4f4af",
+        nome: req.body.nome,
+        login: req.body.login,
+        senha: req.body.senha,
+        saldo: req.body.saldo,
+        adm: req.body.adm,
+        cambista: req.body.cambista,
+        apostas: []
+    });
+    addRefUserById(user.jogo, user._id);
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch {
+        res.status(400).json({ message: "Cannot save it!" });
+    }
+});
+
+// PATCH
+router.patch('/:id', getUser, async (req, res) => {
+    if (req.body.nome != null) {
+        res.user.nome = req.body.nome
+    }
+    try {
+        const updatedUser = await res.user.save();
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// DELETE 
+router.delete('/:id', getUser, async (req, res) => {
+    try {
+        await res.user.remove();
+        res.json({ message: 'User Deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
